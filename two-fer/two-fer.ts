69691ed766c49c8a46fb55b5647f2b836a7118f1
Arthur Mendes <arthurmco@gmail.com>
2023-01-27 11:00:30 -0300

    Solve the second exercise

--- two-fer/two-fer.ts
+++ two-fer/two-fer.ts
@@ -2,7 +2,7 @@
  * This stub is provided to make it straightforward to get started.
  */
 
-export function twoFer(): string {
+export function twoFer(name?: string): string {
   // ^                 ^   ^ this is called a return type; it's the type of the
   // ^                 ^     value that is returned from this function
   // ^                 ^
@@ -10,4 +10,9 @@ export function twoFer(): string {
   // ^
   // allows the tests to import this function and call it
   // <-- Your code goes here. You may remove all the commentary in this file.
+
+  if (name)
+    return `One for ${name}, one for me.`
+  else
+    return `One for you, one for me.`
 }
