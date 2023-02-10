34d8d6740a7f05e4a1c4bc342903cafda92b20a0
Fernandesfelipe1989 <fernandes.felipe1989@gmail.com>
2023-02-10 11:50:00 -0300

    Created the resistor calculate function.

--- resistor-color-trio/resistor-color-trio.ts
+++ resistor-color-trio/resistor-color-trio.ts
@@ -1,27 +1,19 @@
-export function decodedResistorValue(colors: any) {
-  const resistor_colors= {
-    Black: 0,
-    Brown: 1,
-    Red: 2,
-    Orange: 3,
-    Yellow: 4,
-    Green: 5,
-    Blue: 6,
-    Violet: 7,
-    Grey: 8,
-    White: 9,
-  }
-    type Color = keyof typeof resistor_colors
-  function calculateResistor(colors:Color[]){
-    let decodedColor = colors.map(color => resistor_colors[color])
-    const value = Number(`${decodedColor[0]}${decodedColor[1]}`) * 10**decodedColor[2]
-    console.log(value)
-    return value >= 1000 ? `${value/1000} kiloohms` : `${value} ohms`
-  }
-  // const number = Number(`${resistor_colors[colors[0] as keyof typeof resistor_colors]}${resistor_colors[colors[1] as keyof typeof resistor_colors]}`)
-
-  return calculateResistor(colors)
-
-  // throw new Error('Remove this statement and implement this function')
+const resistor_colors= {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
 }
 
+export function decodedResistorValue(colors: any) {
+  const exponential = resistor_colors[colors[2] as keyof typeof resistor_colors]
+  const mult = Number(`${resistor_colors[colors[0] as keyof typeof resistor_colors]}${resistor_colors[colors[1] as keyof typeof resistor_colors]}`)
+  const value = mult * 10 ** exponential
+  return value >= 1000 ? `${value/1000} kiloohms` : `${value} ohms`
+}
