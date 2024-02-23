63f587a237da57f9fa2e08695c39b05e4fcc3cb1
Arthur Mendes <arthurmco@gmail.com>
2024-02-23 11:52:48 -0300

    Solve grade-school exercism

--- grade-school/grade-school.test.ts
+++ grade-school/grade-school.test.ts
@@ -11,14 +11,14 @@ describe('School', () => {
     expect(school.roster()).toEqual({})
   })
 
-  xit('adding a student adds them to the roster for the given grade', () => {
+  it('adding a student adds them to the roster for the given grade', () => {
     school.add('Aimee', 2)
 
     const expectedDb = { 2: ['Aimee'] }
     expect(school.roster()).toEqual(expectedDb)
   })
 
-  xit('adding more students to the same grade adds them to the roster', () => {
+  it('adding more students to the same grade adds them to the roster', () => {
     school.add('Blair', 2)
     school.add('James', 2)
     school.add('Paul', 2)
@@ -27,7 +27,7 @@ describe('School', () => {
     expect(school.roster()).toEqual(expectedDb)
   })
 
-  xit('adding students to different grades adds them to the roster', () => {
+  it('adding students to different grades adds them to the roster', () => {
     school.add('Chelsea', 3)
     school.add('Logan', 7)
 
@@ -35,7 +35,7 @@ describe('School', () => {
     expect(school.roster()).toEqual(expectedDb)
   })
 
-  xit('grade returns the students in that grade in alphabetical order', () => {
+  it('grade returns the students in that grade in alphabetical order', () => {
     school.add('Franklin', 5)
     school.add('Bradley', 5)
     school.add('Jeff', 1)
@@ -44,11 +44,11 @@ describe('School', () => {
     expect(school.grade(5)).toEqual(expectedStudents)
   })
 
-  xit('grade returns an empty array if there are no students in that grade', () => {
+  it('grade returns an empty array if there are no students in that grade', () => {
     expect(school.grade(1)).toEqual([])
   })
 
-  xit('the students names in each grade in the roster are sorted', () => {
+  it('the students names in each grade in the roster are sorted', () => {
     school.add('Jennifer', 4)
     school.add('Kareem', 6)
     school.add('Christopher', 4)
@@ -62,7 +62,7 @@ describe('School', () => {
     expect(school.roster()).toEqual(expectedSortedStudents)
   })
 
-  xit('roster cannot be modified outside of module', () => {
+  it('roster cannot be modified outside of module', () => {
     school.add('Aimee', 2)
     const roster = school.roster()
     try {
@@ -74,7 +74,7 @@ describe('School', () => {
     expect(school.roster()).toEqual(expectedDb)
   })
 
-  xit('roster cannot be modified outside of module using grade()', () => {
+  it('roster cannot be modified outside of module using grade()', () => {
     school.add('Aimee', 2)
     try {
       school.grade(2).push('Oops.')
@@ -85,7 +85,7 @@ describe('School', () => {
     expect(school.roster()).toEqual(expectedDb)
   })
 
-  xit("a student can't be in two different grades", () => {
+  it("a student can't be in two different grades", () => {
     school.add('Aimee', 2)
     school.add('Aimee', 1)
 
--- grade-school/grade-school.ts
+++ grade-school/grade-school.ts
@@ -1,13 +1,38 @@
+type StudentRecord = Record<string, number>
+interface Student {
+  student: string;
+  grade: number
+}
+
 export class GradeSchool {
-  roster() {
-    throw new Error('Remove this statement and implement this function')
+  private students: StudentRecord = {};
+
+  roster(): Record<number, Array<string>> {
+    const grades = new Set(Object.values(this.students));
+    let returnedRoster: Record<number, Array<string>> = {};
+    grades.forEach((g) => {
+      returnedRoster[g] = this.grade(g)
+    })
+    return returnedRoster;
   }
 
-  add() {
-    throw new Error('Remove this statement and implement this function')
+  private toList(): Array<Student> {
+    const pairs = Object.entries(this.students);
+    return pairs.map((pair) => {
+      return {
+        student: pair[0],
+        grade: pair[1]
+      }
+    })
   }
 
-  grade() {
-    throw new Error('Remove this statement and implement this function')
+  add(student: string, grade: number) {
+    this.students[student] = grade;
+  }
+
+  grade(g: number): Array<string> {
+    const students = this.toList().filter(s => s.grade === g).map(s => s.student);
+    students.sort();
+    return students;
   }
 }
